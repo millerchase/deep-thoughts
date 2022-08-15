3b5977e484d1cf9ae00440841e870517d3a97f3d
CMiller <mlr.chase40@gmail.com>
2022-08-14 23:01:58 -0500

    add add user and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
@@ -23,6 +24,30 @@ const resolvers = {
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Invalid credentials');
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError('Invalid credentials');
+      }
+
+      return user;
     }
   }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,6 @@ const { gql } = require('apollo-server-express');
 
 //create our typeDefs
 const typeDefs = gql`
-
   type User {
     _id: ID
     username: String
@@ -34,6 +33,11 @@ const typeDefs = gql`
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 
