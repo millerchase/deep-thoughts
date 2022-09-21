d9b21dfbe3ab9f388830b391f873d76548732fa4
CMiller <mlr.chase40@gmail.com>
2022-09-21 01:38:12 -0500

    setup and connect client/server relationship

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,7 +27,7 @@ const typeDefs = gql`
   }
 
   type Auth {
-    token: ID!
+    token: ID
     user: User
   }
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 // import typeDefs and resolvers
@@ -23,6 +24,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate Apollo server with Express application as middleware
   server.applyMiddleware({ app });
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
