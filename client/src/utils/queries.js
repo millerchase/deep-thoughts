24b9a3d089c31fd63dc87594166f29af9f8d6332
CMiller <mlr.chase40@gmail.com>
2022-10-26 21:31:38 -0500

    implemented user authentication; client side login, user homepage, user profile

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -37,8 +37,56 @@ export const QUERY_THOUGHT = gql`
 `;
 
 export const QUERY_USER = gql`
-query user($username: String!) {
-  user(username: $username) {
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_ME_BASIC = gql `
+{
+  me {
     _id
     username
     email
@@ -47,12 +95,5 @@ query user($username: String!) {
       _id
       username
     }
-    thoughts {
-      _id
-      thoughtText
-      createdAt
-      reactionCount
-    }
   }
-}
-`
+}`
